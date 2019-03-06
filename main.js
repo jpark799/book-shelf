275edef8a8e482e0631935a2a76754f0d0e5ec65
Jason Park <jason.j.park79@gmail.com>
2019-03-06 10:55:39 -0500

    1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var books = [
     title: "Harry Potter",
     author: "J.K. Rowling",
     imageURL: "https://books.google.com/books/content?id=WV8pZj_oNBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-    isnb: "9781921479311",
+    isbn: "9781921479311",
     pageCount: 268
   }
 ];
@@ -12,9 +12,19 @@ var books = [
 // renders books
 var renderBooks = function() {
   $(".books").empty();
-  for (var i = 0; i < books.length; i++) {
-    $(".books").append(books[i].title);
 
+  for (var i = 0; i < books.length; i++) {
+    // $(".books").append(books[i].title);
+    // $(".books").append(books[i].author);
+    // $(".books").append(books[i].isbn);
+    // $(".books").append(books[i].imageURL)
+
+
+
+  var source = $("#book-template").html();
+  var template = Handlebars.compile(source);
+  var newHTML = template({Title: books[i].title, Author: books[i].author});
+  $('.books').append(newHTML);
   }
 };
 
