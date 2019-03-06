bcf4ebda4d6fdcd4f932a18725ad960f0e55f3eb
Jason Park <jason.j.park79@gmail.com>
2019-03-06 13:01:52 -0500

    1

--- main.js
+++ main.js
@@ -8,24 +8,97 @@ var books = [
   }
 ];
 
+var fetch = function (query) {
+  $.ajax({
+    method: "GET",
+    url: "https://www.googleapis.com/books/v1/volumes?q=hunger+games",
+    dataType: "json",
+    success: function(data) {
+      addBooks(data.items);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
 
 // renders books
 var renderBooks = function() {
   $(".books").empty();
 
   for (var i = 0; i < books.length; i++) {
-    // $(".books").append(books[i].title);
-    // $(".books").append(books[i].author);
-    // $(".books").append(books[i].isbn);
-    // $(".books").append(books[i].imageURL)
-
-
-
-  var source = $("#book-template").html();
-  var template = Handlebars.compile(source);
-  var newHTML = template(books[i]);
-  $('.books').append(newHTML);
+    var source = $("#book-template").html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(books[i]);
+    $('.books').append(newHTML);
   }
 };
 
-renderBooks();
+
+var addBooks = function (data) {
+  books = [];
+
+  for (var i = 0; i < data.items.length; i++) {
+    var bookData = data.items[i];
+
+    var author = function () {
+      if (bookData.volumeInfo.authors) {
+        return bookData.volumeInfo.authors[0];
+      } else {
+        return null;
+      }
+    };
+
+    var imageURL = function () {
+      if (bookData.volumeInfo.imageLinks) {
+        return bookData.volumeInfo.imageLinks.thumbnail;
+      } else {
+        return null;
+      }
+    };
+
+    var isbn = function () {
+      if (bookData.volumeInfo.industryIdentifiers) {
+        return bookData.volumeInfo.industryIdentifiers[0].identifier;
+      } else {
+        return null;
+      }
+    };
+
+    var pageCount = function () {
+      if (bookData.volumeInfo.pageCount) {
+        return bookData.volumeInfo.pageCount;
+      } else {
+        return null;
+      }
+    };
+
+    var title = function () {
+      if (bookData.volumeInfo.title) {
+        return bookData.volumeInfo.title;
+      } else {
+        return null;
+      }
+    };
+
+    var book = {
+      title: title(),
+      author: author(),
+      imageURL: imageURL(),
+      pageCount: pageCount(),
+      isbn: isbn()
+    };
+
+    books.push(book);
+  }
+
+  renderBooks();
+};
+
+
+
+$('.search').on('click', function() {
+  var search = $('#searh-query').val();
+
+  fetch(search);
+})
