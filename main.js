5f4e2271d7b8a88940ca8c76045a035232942c21
Jason Park <jason.j.park79@gmail.com>
2019-03-06 10:23:52 -0500

    !

---
+++ main.js
@@ -0,0 +1,18 @@
+var books = [
+  {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    imageURL: "https://books.google.com/books/content?id=WV8pZj_oNBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    isnb: "9781921479311",
+    pageCount: 268
+  }
+];
+
+
+// renders books
+var renderBooks = function() {
+  $(."books").empty();
+  for (var i = 0; i < books.length; i++) {
+
+  }
+};
